cdc5da1017090130728bcb1b9b5d4577536ad6f9
Isaac Carr <isaac.graphicdesign@gmail.com>
2020-06-05 13:23:06 +0100

    removing scroll behaviour1

--- global.js
+++ global.js
@@ -1,38 +1,19 @@
-function EnableAutoLoadMore(buttonElement, contentElement) {
-    $(window).bind('scroll', function() {
-        if (typeof $(buttonElement).first().attr('data-loadmore') === typeof undefined || $(buttonElement).first().attr('data-loadmore') == false) {
-             $(buttonElement).first().attr('data-loadmore', 0);
-        }
-        if ($(buttonElement + ':visible').length > 0) {
-        	if (($(window).height() + $(document).scrollTop()) > ($(buttonElement).first().offset().top - 0) && $(contentElement).first().children().length > $(buttonElement).first().attr('data-loadmore')) {
-                $(buttonElement).first().attr('data-loadmore', $(contentElement).first().children().length);
-                $(buttonElement).click();
-            }
-        }
-    });
-}
-
-$(function() {
-    if (window.location.pathname.substring(0, 7) == '/events' && window.location.pathname.length <= 8) {
-        EnableAutoLoadMore('#load-more-events', '#uc-event-right-panel-listing');
-    } else if (window.location.pathname.substring(0, 7) == '/groups') {
-        EnableAutoLoadMore('#uc-more-group-search', '.category-box-wrapper');
-        $('.club-navigation ul li').click(function() {
-            $('#uc-more-group-search').first().attr('data-loadmore', 0);
-        });
-        $('.group-types-wrapper ul li').click(function() {
-            $('#uc-more-group-search').first().attr('data-loadmore', 0);
-        });
-    } else if (window.location.pathname.substring(0, 9) == '/articles' && window.location.pathname.length <= 10) {
-        EnableAutoLoadMore('#load_more_article', '.uc-articles-listing');
-    } else if (window.location.pathname.substring(0, 16) == '/thestudentvoice' && window.location.pathname.length <= 17) {
-        EnableAutoLoadMore('#load_more_sv', '.uc-sv-left-panel-wrapper');
-        $('#uc-sv-navigation li a').click(function() {
-            $('#load_more_sv').first().attr('data-loadmore', 0);
-        });
-    }
-});
-
-$('html, body').bind('scroll mousedown DOMMouseScroll mousewheel keyup touchstart', function(){
-    $('html, body').stop();
-});
+$(document).unbind().on("click", "#uc-more-group-search", function() {
+    group_type = $(".tree-navigation li.active a").find(".group-type-hidden-id").html(),
+    group_cat = $(".group-types li.active a").find(".group-cat-hidden-id").html(),
+    search_text_to_search = encodeURIComponent($("#search_keyword").val()),
+    "null" == group_type && (group_type = ""),
+    "null" == group_cat && (group_cat = ""),
+    "null" == search_text_to_search && (search_text_to_search = ""),
+    offset += 8,
+    more_groups_path = rewrite_url_for_js("/groups/more_groups?offset=" + offset + "&group_type=" + group_type + "&group_cat=" + group_cat + "&search=" + search_text_to_search),
+    $.ajax({
+        type: "GET",
+        style: "inherit",
+        dataType: "script",
+        url: more_groups_path
+    }).done(function() {
+        $("#uc-more-group-search").show(),
+        "false" == $("#uc-load_search_button").val() && $("#uc-more-group-search").hide()
+    })
+})

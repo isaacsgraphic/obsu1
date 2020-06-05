d096af42f6334a220ce010005bf1994b531c10cb
Isaac Carr <isaac.graphicdesign@gmail.com>
2020-06-05 16:11:40 +0100

    scrolljack4

--- global.js
+++ global.js
@@ -1,24 +1,12 @@
-$(document).unbind().on("click", "#uc-more-group-search", function() {
+$(document).on("click", "#uc-more-group-search", function() {
     //loading another item
-        console.log("twas brillig");
+        console.log("without load");
 });
 
-$(document).unbind().on("click", ".tree-navigation li", function() {
-    //loading another item
-        console.log("tree-navigation li");
-});
+$(document).ready(function(){
 
-$(document).unbind().on("click", ".group-types li", function() {
+    $(document).on("click", "#uc-more-group-search", function() {
     //loading another item
-        console.log("group-types li");
-});
-
-$(document).unbind().on("click", ".search-button", function() {
-    //loading another item
-        console.log("search-button");
-});
-
-$(document).unbind().on("click", "#group_has_overridden_terms_and_conditions", function() {
-    //loading another item
-        console.log("group_has_overridden_terms_and_conditions");
+        console.log("with ready function");
+    });
 });

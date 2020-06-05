71a3cb8fd845f39e63c350d05381eb37348edff5
Isaac Carr <isaac.graphicdesign@gmail.com>
2020-06-05 17:50:08 +0100

    scrolljack9

--- global.js
+++ global.js
@@ -29,21 +29,6 @@ $(document).on("show", "#uc-more-group-search", function() {
 });
 
 
-// $(document).ready(function() {
-//     group_type = $(".tree-navigation li.active a").find(".group-type-hidden-id").html(),
-//     group_cat = $(".group-types li.active a").find(".group-cat-hidden-id").html(),
-//     search_text_to_search = encodeURIComponent($("#search_keyword").val()),
-//     "null" == group_type && (group_type = ""), 
-//     "null" == group_cat && (group_cat = ""), 
-//     "null" == search_text_to_search && (search_text_to_search = ""),
-//     offset += 8, more_groups_path = rewrite_url_for_js("/groups/more_groups?offset=" + offset + "&group_type=" + group_type + "&group_cat=" + group_cat + "&search=" + search_text_to_search),
-//     $.ajax({ 
-//         type: "GET", 
-//         style: "inherit", 
-//         dataType: "script", 
-//         url: more_groups_path 
-//     }).done(function() { 
-//         $("#uc-more-group-search").show(), 
-//         "false" == $("#uc-load_search_button").val() && $("#uc-more-group-search").hide(), 
-//         parseInt($("#group_count").val()) > 0 && $("html, body").animate({ scrollTop: $(".categoryBox.all-groups .uc-group-list-page-wrapper").last().offset().top }, 2e3) })
-// });
+function delay_load_events(){
+    console.log("toast");
+}

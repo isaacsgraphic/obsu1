9234c1695591c841894fe25f0505c40beaa09074
Isaac Carr <isaac.graphicdesign@gmail.com>
2020-06-05 16:22:21 +0100

    scrolljack6

--- global.js
+++ global.js
@@ -4,60 +4,11 @@ $(document).on("click", "#uc-more-group-search", function() {
 });
 
 
-$(document).unbind().on("click", "#uc-more-group-search", function() {
-    //loading another item
-        console.log("twas brillig");
-});
-$(document).unbind().on("click", ".tree-navigation li", function() {
-    //loading another item
-        console.log("tree-navigation li");
-});
-
-$(document).unbind().on("click", ".group-types li", function() {
-    //loading another item
-        console.log("group-types li");
-});
-
-$(document).unbind().on("click", ".search-button", function() {
-    //loading another item
-        console.log("search-button");
-});
-
-$(document).unbind().on("click", "#group_has_overridden_terms_and_conditions", function() {
-    //loading another item
-        console.log("group_has_overridden_terms_and_conditions");
-});
-
 $(document).ready(function(){
 
     $(document).on("click", "#uc-more-group-search", function() {
     //loading another item
         console.log("with ready function");
     });
-
-    $(document).unbind().on("click", "#uc-more-group-search", function() {
-    //loading another item
-        console.log("twas brillig");
-    });
-    $(document).unbind().on("click", ".tree-navigation li", function() {
-        //loading another item
-            console.log("tree-navigation li");
-    });
-
-    $(document).unbind().on("click", ".group-types li", function() {
-        //loading another item
-            console.log("group-types li");
-    });
-
-    $(document).unbind().on("click", ".search-button", function() {
-        //loading another item
-            console.log("search-button");
-    });
-
-    $(document).unbind().on("click", "#group_has_overridden_terms_and_conditions", function() {
-        //loading another item
-            console.log("group_has_overridden_terms_and_conditions");
-    });
-
     
 });

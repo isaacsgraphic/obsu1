8f4cdd517708e22a280b6ea30a33b4cdc533e916
Isaac Carr <isaac.graphicdesign@gmail.com>
2020-06-08 10:50:57 +0100

    scrolljacking8

--- global.js
+++ global.js
@@ -1,18 +1,3 @@
-$(document).on("click", "#uc-more-group-search", function() {
-    //loading another item
-    console.log("without load");
-});
-
-
-$(document).ready(function() {
-
-    $(document).on("click", "#uc-more-group-search", function() {
-        //loading another item
-        console.log("with ready function");
-    });
-
-});
-
 $(document).ready(function(){
     $(document).off("click", "#uc-more-group-search");
 });

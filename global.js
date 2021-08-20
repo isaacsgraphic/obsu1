f47f5b557dbc4ef84237a6b46939326c5f20dd77
Isaac Carr <isaac.graphicdesign@gmail.com>
2021-08-20 10:18:42 +0100

    updating countdown timer

--- global.js
+++ global.js
@@ -232,7 +232,7 @@ $(document).ready(function() {
 //                                      Freshers Countdown Timer
 // -------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("February 17, 2021 17:00:00").getTime();
+var countDownDate = new Date("September 11, 2021 9:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
